ccfeb995c960025da579992819e6f9275aa697ce
Rinaroar <marina.luxin@gmail.com>
2020-03-31 15:12:44 +0200

    finished Home with style

--- portfolio_website_reactjs/src/App.js
+++ portfolio_website_reactjs/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route,} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './contents/Home';
 
 
 function App() {
@@ -9,9 +10,17 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
       </div>
     </Router>
     )
   }
 
 export default App;
+
+
+
+{/* <Route> is the subtag of <Router> or <BrowserRouter>,
+with <Route> we decide what content has to be rendered based on the path/URL. */}
---
+++ portfolio_website_reactjs/src/contents/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import profilePic from '../img/profile.png';
+import Social from '../components/Social';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="condiv home">
+        <img src={profilePic} className="profilePic"></img>
+        <ReactTypingEffect className="typingEffect" text= {['I am Marina Luxin','I am a Web Developer']} speed={100} eraseDelay={700}/>
+        <Social />
+      </div>
+    )
+  }
+}
+export default Home
