e54d2948e0a1f2cac7e2e090c662c3b263f263ec
Rinaroar <marina.luxin@gmail.com>
2020-04-15 17:58:55 +0200

    added resume to download feature, in progress

--- portfolio_website_reactjs/src/contents/Experience.js
+++ portfolio_website_reactjs/src/contents/Experience.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Widecard from '../components/Widecard';
 
+// Create a whole component for the resume feature + add download's link
+
 class Experience extends Component {
     render() {
       return (
@@ -9,6 +11,12 @@ class Experience extends Component {
           <Widecard title="Front-End Developer" where=" SeLoger | Paris" from="September 2019" to="Present" during="Sandwich course in Master Degree"/>
           <Widecard title="Webdesigner | Knowledge Management Assistant" where="Société Générale | Paris" from="2018" to="2019" during="Sandwich course in Bachelor Degree"/>
           <Widecard title="Communication Assistant" where="EDF | Paris" from="2016" to="2018" during="Sandwich course in HND Degree"/>
+          <button class="button-primary resume">
+            <a href="" download><i className="fa fa-chevron-right"></i>Download my resume here</a>
+            <div class="overlay">
+              <div class="text"><i class="far fa-smile"></i><i class="far fa-smile"></i><i class="far fa-smile"></i></div>
+            </div>
+          </button>
         </div>
       )
     }
