360f3088053448be7688c4d0c1d37e94380e090a
Rinaroar <marina.luxin@gmail.com>
2020-04-17 14:54:06 +0200

    Updates

---
+++ precache-manifest.24a402983a102635834a69e734e381b7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de61f3a798158eff5cde09aecc2fd75e",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "02077307c3608be9241d",
+    "url": "/react_portfolio/static/css/main.88dd269c.chunk.css"
+  },
+  {
+    "revision": "b2c96062591b036f01d5",
+    "url": "/react_portfolio/static/js/2.ecb49569.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/react_portfolio/static/js/2.ecb49569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02077307c3608be9241d",
+    "url": "/react_portfolio/static/js/main.ccdc6087.chunk.js"
+  },
+  {
+    "revision": "6cf9c8de65f451f5a585",
+    "url": "/react_portfolio/static/js/runtime-main.b7ec4a6e.js"
+  },
+  {
+    "revision": "ac0e6dd07108c49bb351eeda512c7fc0",
+    "url": "/react_portfolio/static/media/crud_pdo.ac0e6dd0.png"
+  },
+  {
+    "revision": "2dd2193c389f960ba21d9f9e95b56c4b",
+    "url": "/react_portfolio/static/media/exercise.2dd2193c.png"
+  },
+  {
+    "revision": "30fddcd7265804187f3cbfb9c4ea66c4",
+    "url": "/react_portfolio/static/media/front_end_dev.30fddcd7.pdf"
+  },
+  {
+    "revision": "8bebdebb59b572e192681c745aa4a2de",
+    "url": "/react_portfolio/static/media/moveez.8bebdebb.png"
+  },
+  {
+    "revision": "63f86d5bdb19132ace18b5d320815fea",
+    "url": "/react_portfolio/static/media/pink_bc.63f86d5b.jpg"
+  },
+  {
+    "revision": "cfa14dfef1ded88d25f6fe0862b4e14f",
+    "url": "/react_portfolio/static/media/pinkprofile.cfa14dfe.png"
+  },
+  {
+    "revision": "c23f7fec29b1a11056e12944f0dade9a",
+    "url": "/react_portfolio/static/media/seeya.c23f7fec.gif"
+  },
+  {
+    "revision": "c25a8c3628862f902b82d7f009eb0a91",
+    "url": "/react_portfolio/static/media/sunny.c25a8c36.jpg"
+  },
+  {
+    "revision": "db8da81ef528f49cdbd62f801b4a32a4",
+    "url": "/react_portfolio/static/media/wall.db8da81e.png"
+  }
+]);
