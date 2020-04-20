81be31763171aca198fecfac1e5068d28cf26d9e
Rinaroar <marina.luxin@gmail.com>
2020-04-20 18:03:03 +0200

    added a styled component, turned classes into hooks and added  constants files for responsive

--- portfolio_website_reactjs/src/contents/Home.js
+++ portfolio_website_reactjs/src/contents/Home.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import ReactTypingEffect from 'react-typing-effect';
+
 import profilePic from '../img/pinkprofile.png';
 import Social from '../components/Social';
+import { MAX } from "../constants";
 
+const Container = styled.div`
+  padding: 100px ;
+  background-color: #eee;
+  flex: 6;
+`
+const Profile = styled.img`
+  border-radius: 50%;
+  width: 350px;
+  margin-bottom: 30px;
 
-class Home extends Component {
-  render() {
+  ${MAX.MEDIA_M}{
+      margin-bottom: 45px;
+  }
+
+  ${MAX.MEDIA_S}{
+    margin-bottom: 35px;
+}
+`
+
+const Home = () => {
     return (
-      <div className="condiv home">
-        <img src={profilePic} className="profilePic" alt="My face"></img>
+      <Container className="condiv home">
+        <Profile src={profilePic} className="profilePic" alt="My face"></Profile>
         <ReactTypingEffect className="typingEffect" text= {['I am Marina Luxin','I am a Web Developer']} speed={100} eraseDelay={700}/>
         <Social />
-      </div>
+      </Container>
     )
-  }
 }
 export default Home
